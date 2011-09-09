d27388f8a1837f7d878325cfc88022d6d1c8e14d
Jehiah Czebotar <jehiah@gmail.com>
2011-09-09 12:21:18 +0800

    update test code to init/teardown a mongod instance appropriately

--- test/test_safe_updates.py
+++ test/test_safe_updates.py
@@ -7,36 +7,37 @@ import asyncmongo
 
 TEST_TIMESTAMP = int(time.time())
 
-def test_update_safe():
-    test_shunt.setup()
-    db = asyncmongo.Client(pool_id='testinsert', host='127.0.0.1', port=27017, dbname='test', maxconnections=2)
-    
-    def update_callback(response, error):
-        logging.info(response)
-        assert len(response) == 1
-        test_shunt.register_called('update')
-        tornado.ioloop.IOLoop.instance().stop()
-    
-    # all of these should be called, but only one should have a callback
-    # we also are checking that connections in the pool never increases >1 with max_connections=2
-    # this is because connections for safe=False calls get put back in the pool immediated
-    db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
-    db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
-    db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
-    db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
-    db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, upsert=True, callback=update_callback)
-    
-    tornado.ioloop.IOLoop.instance().start()
-    test_shunt.assert_called('update')
-    
-    def query_callback(response, error):
-        logging.info(response)
-        assert isinstance(response, dict)
-        assert response['_id'] == TEST_TIMESTAMP
-        assert response['test_count'] == 5
-        test_shunt.register_called('retrieved')
-        tornado.ioloop.IOLoop.instance().stop()
-    
-    db.test_stats.find_one({"_id" : TEST_TIMESTAMP}, callback=query_callback)
-    tornado.ioloop.IOLoop.instance().start()
-    test_shunt.assert_called('retrieved')
+class SafeUpdatesTest(test_shunt.MongoTest):
+    def test_update_safe(self):
+        test_shunt.setup()
+        db = asyncmongo.Client(pool_id='testinsert', host='127.0.0.1', port=27017, dbname='test', maxconnections=2)
+        
+        def update_callback(response, error):
+            logging.info(response)
+            assert len(response) == 1
+            test_shunt.register_called('update')
+            tornado.ioloop.IOLoop.instance().stop()
+        
+        # all of these should be called, but only one should have a callback
+        # we also are checking that connections in the pool never increases >1 with max_connections=2
+        # this is because connections for safe=False calls get put back in the pool immediated
+        db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
+        db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
+        db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
+        db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, safe=False, upsert=True)
+        db.test_stats.update({"_id" : TEST_TIMESTAMP}, {'$inc' : {'test_count' : 1}}, upsert=True, callback=update_callback)
+        
+        tornado.ioloop.IOLoop.instance().start()
+        test_shunt.assert_called('update')
+        
+        def query_callback(response, error):
+            logging.info(response)
+            assert isinstance(response, dict)
+            assert response['_id'] == TEST_TIMESTAMP
+            assert response['test_count'] == 5
+            test_shunt.register_called('retrieved')
+            tornado.ioloop.IOLoop.instance().stop()
+        
+        db.test_stats.find_one({"_id" : TEST_TIMESTAMP}, callback=query_callback)
+        tornado.ioloop.IOLoop.instance().start()
+        test_shunt.assert_called('retrieved')
--- test/test_shunt.py
+++ test/test_shunt.py
@@ -1,6 +1,10 @@
 import logging
 import sys
 import os
+import unittest
+import subprocess
+import signal
+import time
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
    format='%(asctime)s %(process)d %(filename)s %(lineno)d %(levelname)s #| %(message)s',
@@ -10,13 +14,43 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
 # there is probably a better way to do this that doesn't require magic
 app_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if app_dir not in sys.path:
-    logging.info('adding %r to sys.path' % app_dir)
+    logging.debug('adding %r to sys.path' % app_dir)
     sys.path.insert(0, app_dir)
 
 import asyncmongo
 # make sure we get the local asyncmongo
 assert asyncmongo.__file__.startswith(app_dir)
 
+
+class MongoTest(unittest.TestCase):
+    mongo_options = [('--port', str(27017))]
+    def setUp(self):
+        """setup method that starts up mongod instances using `self.mongo_options`"""
+        self.temp_dirs = []
+        self.mongods = []
+        for options in self.mongo_options:
+            dirname = os.tempnam()
+            os.makedirs(dirname)
+            self.temp_dirs.append(dirname)
+            
+            options = ['mongod', '--bind_ip', '127.0.0.1', '--oplogSize', '10', '--dbpath', dirname] + list(options)
+            logging.debug(options)
+            pipe = subprocess.Popen(options)
+            self.mongods.append(pipe)
+            logging.debug('started mongod %s' % pipe.pid)
+        logging.info('waiting for mongod to start')
+        time.sleep(3)
+    
+    def tearDown(self):
+        """teardown method that cleans up child mongod instances, and removes their temporary data files"""
+        for mongod in self.mongods:
+            logging.debug('killing mongod %s' % mongod.pid)
+            os.kill(mongod.pid, signal.SIGKILL)
+            mongod.wait()
+        for dirname in self.temp_dirs:
+            logging.debug('cleaning up %s' % dirname)
+            pipe = subprocess.Popen(['rm', '-rf', dirname])
+            pipe.wait()
 
 results = {}
 
