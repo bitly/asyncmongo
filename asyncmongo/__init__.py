faff840fd7bcffd497526f272afd384efdc9c425
Jehiah Czebotar <jehiah@gmail.com>
2011-10-21 17:30:54 -0400

    bump version to 1.0.3

--- asyncmongo/__init__.py
+++ asyncmongo/__init__.py
@@ -29,8 +29,8 @@ try:
 except ImportError:
     raise ImportError("tornado library not installed. Install tornado. https://github.com/facebook/tornado")
 
-version = "1.0"
-version_info = (1, 0)
+version = "1.0.3"
+version_info = (1, 0, 3)
 
 ASCENDING = 1
 """Ascending sort order."""
